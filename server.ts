c320b32f9c4d6d2a11d692485c65818349ec5edb
GlitchyCzE <cze@clashmc.co>
2021-02-20 13:15:16 -0500

    Created entry point config check

--- server.ts
+++ server.ts
@@ -1,2 +1,11 @@
 // Entry point of server
+
+// Execute check for Config.ts
+import * as fs from "fs";
+if (!fs.existsSync("./app/config.ts")) {
+    console.log("Could not find ./app/config.ts! (Did you copy it from config.ts.example?)");
+    process.exit(1);
+}
+
+// Start webserver
 import("./app/web/express")
