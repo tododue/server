08669392639ad77f038b5e1ac00dcbea757a00fc
GlitchyCzE <cze@clashmc.co>
2021-02-20 15:04:50 -0500

    Added new ORMs to DBM

--- app/db/dbManager.ts
+++ app/db/dbManager.ts
@@ -2,6 +2,8 @@ import {Connection, createConnection} from "typeorm";
 import {Config} from "../config";
 import {Log} from "../log";
 import {User} from "./orm/user";
+import {Assignment} from "./orm/assignment";
+import {Class} from "./orm/class";
 
 export class DbManager {
 
@@ -16,7 +18,7 @@ export class DbManager {
             database: Config.DB_DATABASE,
             username: Config.DB_USER,
             password: Config.DB_PASSWORD,
-            entities: [User],
+            entities: [User, Assignment, Class],
             synchronize: true,
             logging: Config.DEBUG_MODE
         }).catch(error => {
@@ -24,13 +26,21 @@ export class DbManager {
             console.log(error);
             process.exit(1);
         }).then(connection => {
-           this.sqlConnection = connection;
-           Log.info("DBM", `Service Online! SQL Connection '${this.sqlConnection.name}' operational`);
+            this.sqlConnection = connection;
+            Log.info("DBM", `Service Online! SQL Connection '${this.sqlConnection.name}' operational`);
         });
     }
 
     getUsers() {
         return this.sqlConnection.getRepository(User);
+    }
+
+    getAssignments() {
+        return this.sqlConnection.getRepository(Assignment);
+    }
+
+    getClasses() {
+        return this.sqlConnection.getRepository(Class);
     }
 
 
