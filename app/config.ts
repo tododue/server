06fac0dc33d4ce1d5c3d4a8b0d551bf7dd5a6792
GlitchyCzE <cze@clashmc.co>
2021-02-20 13:24:27 -0500

    Created customized config system

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,21 @@
+import * as fs from "fs";
+
+// Read config file
+let configFile = fs.readFileSync("config.json");
+
+// @ts-ignore JSON parse takes buffers but ts says it can't?
+let config = JSON.parse(configFile);
+
 export class Config {
 
-    static APP_ADDRESS = "0.0.0.0";
-    static APP_PORT = 3000;
+    static APP_ADDRESS = config["APP_ADDRESS"];
+    static APP_PORT = config["APP_PORT"];
+    static DB_HOST = config["DB_HOST"];
+    static DB_PORT = config["DB_PORT"];
+    static DB_DATABASE = config["DB_DATABASE"];
+    static DB_USER = config["DB_USER"];
+    static DB_PASSWORD = config["DB_PASSWORD"];
+
     static ROOT = __dirname;
 
 }
--- server.ts
+++ server.ts
@@ -2,8 +2,8 @@
 
 // Execute check for Config.ts
 import * as fs from "fs";
-if (!fs.existsSync("./app/config.ts")) {
-    console.log("Could not find ./app/config.ts! (Did you copy it from config.ts.example?)");
+if (!fs.existsSync("./app/config.json")) {
+    console.log("Could not find ./app/config.json! (Did you copy it from config.json.example?)");
     process.exit(1);
 }
 
