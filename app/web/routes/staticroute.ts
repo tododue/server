3a329a067e8dcb6865d85266fce3dfe0373cfb4d
GlitchyCzE <cze@clashmc.co>
2021-02-20 12:56:37 -0500

    Created static public route

--- app/web/routes/staticroute.ts
+++ app/web/routes/staticroute.ts
@@ -1,10 +1,12 @@
 import {ExpressWithAsync} from "@awaitjs/express";
+import * as express from "express";
+import * as path from "path";
 
 export class StaticRoute {
 
     // Application route for static services such as the main page and public assets
     static route(server: ExpressWithAsync) {
-
+        server.use(express.static(path.join(__dirname, "../../public")));
     }
 
 }
