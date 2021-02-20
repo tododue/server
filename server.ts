bf8f67e126de00f3f59ac08132ad047408bb7a7a
GlitchyCzE <cze@clashmc.co>
2021-02-20 14:10:46 -0500

    Reformatted server.ts and initialized DBM

--- server.ts
+++ server.ts
@@ -1,11 +1,18 @@
 // Entry point of server
 
-// Execute check for Config.ts
 import * as fs from "fs";
+import {DBManager} from "./app/db/dbmanager";
+
+// Execute check for config.json
 if (!fs.existsSync("./config.json")) {
     console.error("Could not find ./config.json! (Did you copy it from config.json.example?)");
     process.exit(1);
 }
+
+// Start Database Manager
+let DBM = new DBManager();
+DBManager.DBM = DBM;
+DBM.init();
 
 // Start webserver
 import("./app/web/express")
