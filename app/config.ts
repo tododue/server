a9fabd35597fe32a09ff51af020cef35930c9b2d
GlitchyCzE <cze@clashmc.co>
2021-02-20 13:07:18 -0500

    Setup routing for static content

--- app/config.ts
+++ app/config.ts
@@ -2,5 +2,6 @@ export class Config {
 
     static APP_ADDRESS = "0.0.0.0";
     static APP_PORT = 3000;
+    static ROOT = __dirname;
 
 }
--- app/web/routes/staticroute.ts
+++ app/web/routes/staticroute.ts
@@ -1,12 +1,21 @@
 import {ExpressWithAsync} from "@awaitjs/express";
 import * as express from "express";
 import * as path from "path";
+import {Config} from "../../config";
 
 export class StaticRoute {
 
     // Application route for static services such as the main page and public assets
     static route(server: ExpressWithAsync) {
-        server.use(express.static(path.join(__dirname, "../../public")));
+
+        // All static content served from index.html
+        server.use(express.static(path.join(Config.ROOT, "/public")));
+
+        // Send the index.html if they request for /
+        server.get("/", (req, res) => {
+           res.sendFile(path.join(Config.ROOT, "/public/index.html"));
+        });
+
     }
 
 }
