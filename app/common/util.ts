c0dd7a18e7e083a9cc5b389175f91245111586d3
GlitchyCzE <cze@clashmc.co>
2021-02-21 10:33:28 -0500

    Created parserWebWork

--- app/common/util.ts
+++ app/common/util.ts
@@ -6,4 +6,9 @@ export class Util {
         return crypto.randomBytes(size).toString('hex');
     }
 
+    static convertTZ(date, tzString) {
+        return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));
+    }
+
+
 }
