0e12cf36cf2238f8f158342c833d9dbd9a406b28
GlitchyCzE <cze@clashmc.co>
2021-02-20 19:53:24 -0500

    Laid foundations for /api/upload

--- app/web/routes/api/apiAssignmentRoute.ts
+++ app/web/routes/api/apiAssignmentRoute.ts
@@ -6,6 +6,13 @@ export class ApiAssignmentRoute {
     static route(server: ExpressWithAsync) {
         Log.info("EXPRESS", "Registering ApiAssignmentRoute...");
 
+        // Upload of an assignment page by the extension
+        server.postAsync("/api/upload", (req, res) => {
+            let platform = req.body["platform"];
+            let page = req.body["page"];
 
+            // parser
+
+        });
     }
 }
