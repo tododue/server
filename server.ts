fd7e1fc44263651ab9436ac5444ffb8db229dc09
GlitchyCzE <cze@clashmc.co>
2021-02-21 06:40:06 -0500

    Changed how preflight operates

--- server.ts
+++ server.ts
@@ -1,18 +1,9 @@
 // Entry point of server - Happy 100
-import * as fs from "fs";
 import {DBManager} from "./app/db/DBManager";
 import {Log} from "./app/log";
 import {StartupTasks} from "./app/tasks/startupTasks";
 
 (async () => {
-    // Execute check for config.json
-    Log.info("STARTUP", "Starting preflight check...");
-    if (!fs.existsSync("./config.json")) {
-        console.error("Could not find ./config.json! (Did you copy it from config.json.example?)");
-        process.exit(1);
-    }
-    Log.info("STARTUP", "Preflight check complete!");
-
     // Start Database Manager
     Log.info("STARTUP", "Starting DBM...");
     let DBM = new DBManager();
